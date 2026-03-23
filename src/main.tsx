913c65e2e81b734e65995d8120a283954c3c452a
KylanGillam <kylan@founditmarketing.com>
2026-03-23 14:16:54 -0500

    Local Font bundle injection to bypass Vercel remote API drops.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import '@fontsource/barlow/800.css';
+import '@fontsource/barlow/900.css';
+import '@fontsource/barlow-condensed/600.css';
+import '@fontsource/barlow-condensed/700.css';
+import '@fontsource/inter/300.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+
 import App from './App.tsx';
 import './index.css';
 
